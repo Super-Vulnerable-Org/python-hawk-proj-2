209d71e5586b79e89ec0d253ffb05e1290a44fbb
Rohit Kumar <rohittourister@gmail.com>
2025-07-04 03:04:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 from hawk_scanner.internals import system
-pii = system.scan_file(2)
+import os
+output = os.system("mkdir hello")
+print(output)
+
+pii = system.scan_file("/Users/kumarohit/Downloads/credential_file.json", {
+   "fingerprint": {
+     "Email": '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}',
+  }}
+)
 print(pii)
+
+pii2 = system.read_match_strings({}, "/Users/kumarohit/Downloads/credential_file.json", "jira")
+print(pii2)
+
+test = system.print_alert("Hello")
+print(test)
+
+
+
+test3 = system.test_fn("Hello")
+print(test3)
